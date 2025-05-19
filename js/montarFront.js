ca93cb51d6ddbbbdaa894476f12279e707d0ad66
Andrey <andreydavi3ao@gmail.com>
2025-05-19 11:52:49 -0300

    08 - criando o arquivo montarFront.js - usando o objeto Shows - Linkando com a área de próximos shows do menu

---
+++ js/montarFront.js
@@ -0,0 +1,34 @@
+import { shows } from "./objetos.js";
+
+// Montando o grid de shows
+//passando o grid shows para JS, será nele q os cards irão ficar
+const gridShows = document.querySelector('#gridShows');
+
+// montando os shows dentro dos cards
+shows.forEach(show => {
+    gridShows.innerHTML += `
+            <div class="col-md-6 col-lg-4 my-3>
+                <div class="card">
+                    <a href="" class="text-decoration-none text-dark">
+                    <img src="${show.banner}" class="card-img-top" 
+                    alt="${show.artista}">
+                    <div class="card-body">
+
+                            <h4 class="my-3 bd-bottom">${show.artista}</h4>
+                            <div class="d-flex justify-content-between">
+                                <p>Local: ${show.local}</p>
+                                <p>Data: ${show.data}</p>
+                            </div>
+                            <div class="d-flex justify-content-between">
+                                <p>Horário: ${show.horario}</p>
+                                <p>Ingresso: ${show.tipo_ingresso}</p>
+                            </div>
+                    </div>
+                    </a>
+                </div>
+            </div>
+    
+    
+    
+    `
+})
---
+++ js/objetos.js
@@ -0,0 +1,246 @@
+export const shows = [
+    {
+        artista: "Foo Fighters",
+        local: "Brasília",
+        data: "2025-06-10",
+        horario: "21:00",
+        tipo_ingresso: "inteira",
+        banner: "../images/image0.jpg"
+    },
+    {
+        artista: "Red Hot Chili Peppers",
+        local: "São Paulo",
+        data: "2025-06-15",
+        horario: "21:00",
+        tipo_ingresso: "meia entrada",
+        banner: "../images/image1.jpg"
+    },
+    {
+        artista: "Pearl Jam",
+        local: "Rio de Janeiro",
+        data: "2025-06-20",
+        horario: "21:00",
+        tipo_ingresso: "premium",
+        banner: "../images/image2.jpg"
+    },
+    {
+        artista: "Nirvana",
+        local: "Belo Horizonte",
+        data: "2025-06-25",
+        horario: "21:00",
+        tipo_ingresso: "pista",
+        banner: "../images/image3.jpg"
+    },
+    {
+        artista: "The Strokes",
+        local: "Porto Alegre",
+        data: "2025-07-01",
+        horario: "21:00",
+        tipo_ingresso: "VIP",
+        banner: "../images/image4.jpg"
+    },
+    {
+        artista: "Queens of the Stone Age",
+        local: "Recife",
+        data: "2025-07-05",
+        horario: "21:00",
+        tipo_ingresso: "meia entrada",
+        banner: "../images/image5.jpg"
+    },
+    {
+        artista: "Arctic Monkeys",
+        local: "Salvador",
+        data: "2025-07-10",
+        horario: "21:00",
+        tipo_ingresso: "inteira",
+        banner: "../images/image6.jpg"
+    },
+    {
+        artista: "Muse",
+        local: "Fortaleza",
+        data: "2025-07-15",
+        horario: "21:00",
+        tipo_ingresso: "premium",
+        banner: "../images/image7.jpg"
+    },
+    {
+        artista: "Radiohead",
+        local: "Manaus",
+        data: "2025-07-20",
+        horario: "21:00",
+        tipo_ingresso: "pista",
+        banner: "../images/image8.jpg"
+    },
+    {
+        artista: "The Killers",
+        local: "Curitiba",
+        data: "2025-07-25",
+        horario: "21:00",
+        tipo_ingresso: "VIP",
+        banner: "../images/image9.jpg"
+    },
+    {
+        artista: "Linkin Park",
+        local: "Palmas",
+        data: "2025-07-30",
+        horario: "21:00",
+        tipo_ingresso: "meia entrada",
+        banner: "../images/image1.jpg"
+    },
+    {
+        artista: "Green Day",
+        local: "São Luís",
+        data: "2025-08-05",
+        horario: "21:00",
+        tipo_ingresso: "inteira",
+        banner: "../images/image2.jpg"
+    },
+    {
+        artista: "Imagine Dragons",
+        local: "Teresina",
+        data: "2025-08-10",
+        horario: "21:00",
+        tipo_ingresso: "premium",
+        banner: "../images/image3.jpg"
+    },
+    {
+        artista: "The Smashing Pumpkins",
+        local: "João Pessoa",
+        data: "2025-08-15",
+        horario: "21:00",
+        tipo_ingresso: "VIP",
+        banner: "../images/image4.jpg"
+    },
+    {
+        artista: "Paramore",
+        local: "Natal",
+        data: "2025-08-20",
+        horario: "21:00",
+        tipo_ingresso: "pista",
+        banner: "../images/image5.jpg"
+    },
+    {
+        artista: "Coldplay",
+        local: "Aracaju",
+        data: "2025-08-25",
+        horario: "21:00",
+        tipo_ingresso: "meia entrada",
+        banner: "../images/image6.jpg"
+    },
+    {
+        artista: "The Rolling Stones",
+        local: "Maceió",
+        data: "2025-09-01",
+        horario: "21:00",
+        tipo_ingresso: "inteira",
+        banner: "../images/image7.jpg"
+    },
+    {
+        artista: "Bon Jovi",
+        local: "Cuiabá",
+        data: "2025-09-05",
+        horario: "21:00",
+        tipo_ingresso: "VIP",
+        banner: "../images/image8.jpg"
+    },
+    {
+        artista: "The Who",
+        local: "Campo Grande",
+        data: "2025-09-10",
+        horario: "21:00",
+        tipo_ingresso: "pista",
+        banner: "../images/image9.jpg"
+    },
+    {
+        artista: "U2",
+        local: "Macapá",
+        data: "2025-09-15",
+        horario: "21:00",
+        tipo_ingresso: "premium",
+        banner: "../images/image1.jpg"
+    },
+    {
+        artista: "Blink-182",
+        local: "Boa Vista",
+        data: "2025-09-20",
+        horario: "21:00",
+        tipo_ingresso: "meia entrada",
+        banner: "../images/image2.jpg"
+    },
+    {
+        artista: "Oasis",
+        local: "Rio Branco",
+        data: "2025-09-25",
+        horario: "21:00",
+        tipo_ingresso: "VIP",
+        banner: "../images/image3.jpg"
+    },
+    {
+        artista: "The Doors",
+        local: "Vitória",
+        data: "2025-09-30",
+        horario: "21:00",
+        tipo_ingresso: "inteira",
+        banner: "../images/image4.jpg"
+    },
+    {
+        artista: "Led Zeppelin",
+        local: "Belém",
+        data: "2025-10-05",
+        horario: "21:00",
+        tipo_ingresso: "premium",
+        banner: "../images/image5.jpg"
+    },
+    {
+        artista: "Pink Floyd",
+        local: "Florianópolis",
+        data: "2025-10-10",
+        horario: "21:00",
+        tipo_ingresso: "pista",
+        banner: "../images/image6.jpg"
+    },
+    {
+        artista: "The Beatles",
+        local: "Goiânia",
+        data: "2025-10-15",
+        horario: "21:00",
+        tipo_ingresso: "meia entrada",
+        banner: "../images/image7.jpg"
+    },
+    {
+        artista: "Kings of Leon",
+        local: "São Paulo",
+        data: "2025-10-20",
+        horario: "21:00",
+        tipo_ingresso: "inteira",
+        banner: "../images/image8.jpg"
+    },
+    {
+        artista: "The Black Keys",
+        local: "Belo Horizonte",
+        data: "2025-10-25",
+        horario: "21:00",
+        tipo_ingresso: "VIP",
+        banner: "../images/image9.jpg"
+    },
+    {
+        artista: "AC/DC",
+        local: "Rio de Janeiro",
+        data: "2025-11-01",
+        horario: "21:00",
+        tipo_ingresso: "premium",
+        banner: "../images/image1.jpg"
+    },
+    {
+        artista: "System of a Down",
+        local: "Fortaleza",
+        data: "2025-11-05",
+        horario: "21:00",
+        tipo_ingresso: "pista",
+        banner: "../images/image2.jpg"
+    }
+];
+
+
+
+  
